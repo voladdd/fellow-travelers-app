4003d5282590e31ab6f2dd657e11b85d0e304cb7
Vlad <79471942+voladdd@users.noreply.github.com>
2023-02-17 18:13:32 +0300

    adding transport

--- src/tours/controllers/transports.controller.ts
+++ src/tours/controllers/transports.controller.ts
@@ -1,5 +1,13 @@
-import { Controller, Get, HttpException, HttpStatus } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpException,
+  HttpStatus,
+  Post,
+} from '@nestjs/common';
 import { TransportsService } from '../services/transports.service';
+import { CreateTransportDto } from '../dto/create-transport.dto';
 
 @Controller('tours')
 export class TransportsController {
@@ -9,6 +17,16 @@ export class TransportsController {
   async findAll() {
     try {
       return await this.transportsService.findAll();
+    } catch (error) {
+      console.log(error._message);
+      throw new HttpException('Error', HttpStatus.BAD_REQUEST);
+    }
+  }
+
+  @Post('/transports')
+  async create(@Body() createTransportDto: CreateTransportDto) {
+    try {
+      return await this.transportsService.create(createTransportDto);
     } catch (error) {
       console.log(error._message);
       throw new HttpException('Error', HttpStatus.BAD_REQUEST);
--- src/tours/services/transports.service.ts
+++ src/tours/services/transports.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Transport, TransportDocument } from '../schemas/transport.schema';
+import { CreateTransportDto } from '../dto/create-transport.dto';
 
 @Injectable()
 export class TransportsService {
@@ -12,5 +13,10 @@ export class TransportsService {
 
   async findAll(): Promise<Transport[]> {
     return await this.transportModel.find();
+  }
+
+  async create(createTransportDto: CreateTransportDto): Promise<Transport> {
+    const transport = new this.transportModel(createTransportDto);
+    return await transport.save();
   }
 }
