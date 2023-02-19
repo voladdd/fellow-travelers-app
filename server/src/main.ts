6e2dcd36c75b067aea191a8570711357a169055f
Vlad <79471942+voladdd@users.noreply.github.com>
2023-02-19 14:13:13 +0300

    changed port for server

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,7 +4,10 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const port = process.env.PORT;
   app.useGlobalPipes(new ValidationPipe({ validateCustomDecorators: true }));
-  await app.listen(3000);
+  await app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+  });
 }
 bootstrap();
